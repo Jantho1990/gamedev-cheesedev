afb537fe7799d075da47d31f067ac13551a7cb57
Joshua Anthony <jantho1990@gmail.com>
2018-08-13 12:02:15 -0500

    moved collision detection functionality to entity helpers; added lots of cheese

--- pop/utils/entity.js
+++ pop/utils/entity.js
@@ -15,6 +15,17 @@ export function addDebug(e) {
   return e
 }
 
+export function bounds(entity) {
+  const { w, h, pos, hitBox } = entity
+  const hit = hitBox || { x: 0, y: 0, w, h }
+  return {
+    x: hit.x + pos.x,
+    y: hit.y + pos.y,
+    w: hit.w - 1,
+    h: hit.h - 1
+  }
+}
+
 export function center(entity) {
   const { pos, w, h } = entity
   return {
@@ -27,8 +38,19 @@ export function distance(a, b) {
   return math.distance(center(a), center(b))
 }
 
+export function hit(e1, e2) {
+  const a = bounds(e1)
+  const b = bounds(e2)
+  return a.x + a.w >= b.x && // e1 right edge overlaps e2 left edge
+    a.x <= b.x + b.w && // e1 left edge overlaps e2 right edge
+    a.y + a.h >= b.y && // e1 bottom edge overlaps e2 top edge
+    a.y <= b.y + b.h // e1 top edge overlaps e2 bottom edge 
+}
+
 export default {
   addDebug,
+  bounds,
   center,
-  distance
+  distance,
+  hit
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import pop from '../pop'
 import Cheese from './entities/Cheese'
 import Mouse from './entities/Mouse'
+import Container from '../pop/Container';
 
 const { Game, KeyControls, entity, math } = pop
 
 const game = new Game(640, 320)
 const { scene, w, h } = game
-
-const mouse = scene.add(new Mouse(new KeyControls()))
-const cheese = scene.add(new Cheese())
 
 const relocate = e => {
   const { pos } = e
@@ -16,37 +14,22 @@ const relocate = e => {
   pos.y = math.rand(h)
 }
 
+const mouse = scene.add(new Mouse(new KeyControls()))
 relocate(mouse)
-relocate(cheese)
-
 entity.addDebug(mouse)
-entity.addDebug(cheese)
+
+const cheeses = scene.add(new Container())
+for (let i = 0; i < 10; i++) {
+  const cheese = cheeses.add(new Cheese())
+  relocate(cheese)
+  entity.addDebug(cheese)
+}
 
 game.run(() => {
-  const { hitBox: aHit, pos: aPos } = mouse
-  const a = {
-    x: aHit.x + aPos.x,
-    y: aHit.y + aPos.y,
-    w: aHit.w,
-    h: aHit.h
-  }
-
-  const { hitBox: bHit, pos: bPos } = cheese
-  const b = {
-    x: bHit.x + bPos.x,
-    y: bHit.y + bPos.y,
-    w: bHit.w,
-    h: bHit.h
-  }
-
-  // Bounding box detection
-  if (
-    a.x + a.w >= b.x && // Mouse right edge overlaps cheese left edge
-    a.x < b.x + b.w && // Mouse left edge overlaps cheese right edge
-    a.y + a.h >= b.y && // Mouse bottom edge overlaps cheese top edge
-    a.y < b.y + b.h // Mouse top edge overlaps cheese bottom edge
-  ) {
-    // Hit!
-    relocate(cheese)
-  }
+  cheeses.map(cheese => {
+    // Check for collisions with cheese
+    if (entity.hit(mouse, cheese)) {
+      relocate(cheese)
+    }
+  })
 })
